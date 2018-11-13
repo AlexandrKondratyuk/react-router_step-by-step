b911946a245435cbe752eaf21dfb18990be3988f
Oleksandr <sankoniks@gmail.com>
2018-11-14 00:28:21 +0200

    imported & configured react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Router} from 'react-router-dom'
 import {BrowserRouter} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
-
-const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const application = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(application, document.getElementById('root'));
 registerServiceWorker();
