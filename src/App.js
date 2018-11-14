7e1250ef4bc0c2493b43000a3a1b26ffc3f42332
Oleksandr <sankoniks@gmail.com>
2018-11-14 10:47:45 +0200

    added NavLink for links for navigations between pages without reloading 2

--- src/App.js
+++ src/App.js
@@ -5,33 +5,52 @@ import About from './About/About'
 import Cars from './Cars/Cars'
 
 class App extends Component {
-  render() {
+    render() {
 
-    return (
-      <div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-              <li>
-              <NavLink to="/cars">Cars</NavLink>
-            </li>
-          </ul>
-        </nav>
+        return (
+            <div>
+                <nav className="nav">
+                    <ul>
+                        <li>
+                            <NavLink
+                                to="/"
+                                exact
+                                activeClassName={'wfn-active'}>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/about"
+                            activeStyle={{
+                                color: 'blue'
+                            }}>
+                                About
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to={{
+                                    pathname: 'cars',
+                                    search: '?a=1&b=2',
+                                    hash: 'wfm-cash'
+                                }}
+                            >
+                                Cars
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
 
-        <hr/>
+                <hr/>
 
-          <Route path='/' exact render={() => <h1>Home page</h1>} />
-          <Route path='/about' component={About}/>
-          <Route path='/cars' component={Cars}/>
+                <Route path='/' exact render={() => <h1>Home page</h1>}/>
+                <Route path='/about' component={About}/>
+                <Route path='/cars' component={Cars}/>
 
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App
